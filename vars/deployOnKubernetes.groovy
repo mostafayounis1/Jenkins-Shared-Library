d89b0486886e52541d6f87371d5404cc8011a83b
mostafayounis1 <167571650+mostafayounis1@users.noreply.github.com>
2024-05-04 22:04:10 +0300

    Update deployOnKubernetes.groovy
    Signed-off-by: mostafayounis1 <167571650+mostafayounis1@users.noreply.github.com>

--- vars/deployOnKubernetes.groovy
+++ vars/deployOnKubernetes.groovy
@@ -7,6 +7,6 @@ def call(String k8sCredentialsID) {
 
     // login to k8s Cluster via KubeConfig file
     withCredentials([file(credentialsId: "${k8sCredentialsID}", variable: 'KUBECONFIG_FILE')]) {
-        sh "sudo export KUBECONFIG=${KUBECONFIG_FILE} && kubectl apply -f ."
+        sh "export KUBECONFIG=${KUBECONFIG_FILE} && kubectl apply -f ."
     }
 }
